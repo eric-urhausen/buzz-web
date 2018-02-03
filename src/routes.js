e09662a81703394f693d4a4ed9ab1706f02d3e35
Eric Urhausen <ericurhausen@gmail.com>
2018-02-02 18:37:11 -0800

    Initial Commit

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+/* @flow */
+
+import type { Dispatch } from './types';
+import { fetchUsersIfNeeded } from './actions/users';
+import { fetchUserIfNeeded } from './actions/user';
+import { HomePage, UserInfoPage, NotFoundPage } from './containers';
+
+export default [
+  {
+    path: '/',
+    exact: true,
+    component: HomePage, // Add your route here
+    loadData: (dispatch: Dispatch) =>
+      Promise.all([
+        dispatch(fetchUsersIfNeeded()) // Register your server-side call action(s) here
+      ])
+  },
+  {
+    path: '/UserInfo/:id',
+    component: UserInfoPage,
+    loadData: (dispatch: Dispatch, params: Object) =>
+      Promise.all([dispatch(fetchUserIfNeeded(params.id))])
+  },
+  {
+    component: NotFoundPage
+  }
+];
